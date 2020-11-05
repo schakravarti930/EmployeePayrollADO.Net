200d6af888f28349f2c319c53d1bfa8714ae6446
Swapnil <schakravarti930@gmail.com>
2020-11-05 20:02:05 +0530

    [Swapnil] Add : Connect to DataBase using ADO.Net

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=DESKTOP-1KFT47J\SQLEXPRESS;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
